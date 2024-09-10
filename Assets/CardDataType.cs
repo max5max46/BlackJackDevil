aa1acdb1f4e436c1f0f4636448b76ada0e00c0ee
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2024-09-10 10:45:48 -0300

    Initial commit

---
+++ Assets/CardDataType.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CardDataType
+{
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+
+    public struct Card
+    {
+        public int number;
+        public Suit suit;
+        public string suitName;
+
+        public Card(int number = 1, Suit suit = Suit.Hearts)
+        {
+            this.number = number;
+            this.suit = suit;
+
+            switch (suit)
+            {
+                case Suit.Hearts:
+                    suitName = "Hearts";
+                    break;
+
+                case Suit.Diamonds:
+                    suitName = "Diamonds";
+                    break;
+
+                case Suit.Clubs:
+                    suitName = "Clubs";
+                    break;
+
+                case Suit.Spades:
+                    suitName = "Spades";
+                    break;
+
+                default:
+                    suitName = "Error";
+                    break;
+            }
+        }
+
+        public void DebugShowCard()
+        {
+            Debug.Log("Card is a " + number + " of " + suitName);
+        }
+    }
+}
---
+++ Assets/Deck.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private List<CardDataType.Card> deck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deck = new List<CardDataType.Card>();
+
+        // Creates a full deck of 52 cards
+        for (int num = 1; num <= 13; num++)
+            for (int suit = 0; suit < 4; suit++)
+                deck.Add(new CardDataType.Card(num, (CardDataType.Suit)suit));
+
+        ShuffleDeck();
+
+        for (int x = 0; x < deck.Count; x++)
+            deck[x].DebugShowCard();
+    }
+
+    // Draws a card from the top of the pile (index 51, when at full deck) and returns its value while removing that same card from the deck
+    public void AddCard(CardDataType.Card card)
+    {
+        if (deck.Count == 0)
+            Debug.LogError("Tried to Draw Card while deck was Empty");
+
+        CardDataType.Card tempCard;
+
+        tempCard = deck[deck.Count - 1];
+        deck.Remove(tempCard);
+
+        if (deck.Count == 53)
+            Debug.LogError("Deck exceeded max limit of 52");
+
+    }
+
+    // Draws a card from the top of the pile (index 51, when at full deck) and returns its value while removing that same card from the deck
+    public CardDataType.Card DrawCard()
+    {
+        if (deck.Count == 0)
+            Debug.LogError("Tried to Draw Card while deck was Empty");
+
+        CardDataType.Card tempCard;
+
+        tempCard = deck[deck.Count - 1];
+        deck.Remove(tempCard);
+
+        return tempCard;
+    }
+
+
+    // Shuffles the current deck regardless of size
+    public void ShuffleDeck()
+    {
+        List<CardDataType.Card> tempDeck = new List<CardDataType.Card>();
+        CardDataType.Card tempCard;
+
+        while (deck.Count > 0)
+        {
+            tempDeck.Add(deck[0]);
+            deck.Remove(deck[0]);
+        }
+
+        while (tempDeck.Count > 0)
+        {
+            tempCard = tempDeck[Random.Range(0, tempDeck.Count - 1)];
+
+            deck.Add(tempCard);
+            tempDeck.Remove(tempCard);
+        }
+
+    }
+
+}
